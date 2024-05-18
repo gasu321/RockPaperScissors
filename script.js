894197537354808311f8632a2a9447c5d5994033
gasu321 <161547607+gasu321@users.noreply.github.com>
2024-05-18 01:30:10 -0700

    add function to prompt player for an answer

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
 function getComputerChoice() {
-    let gameRoll = Math.random() * 100;
-    if (gameRoll <= 33) {
-        return "Rock"
-    } else if (gameRoll <= 66 & gameRoll > 33) {
-        return "Paper"
+    let computerRoll = Math.random() * 100;
+    if (computerRoll <= 33) {
+        return "rock"
+    } else if (computerRoll <= 66 & computerRoll > 33) {
+        return "paper"
     } else {
-        return "Scissors"
+        return "scissors"
     }
 }
 
 console.log(getComputerChoice());
+
+function getHumanChoice() {
+    let choice = prompt("Rock Paper or Scissors?").toLowerCase();
+    if (choice === "rock") {
+        return "rock";
+    } else if (choice === "paper") {
+        return "paper";
+    } else if (choice === "scissors") {
+        return "scissors";
+    } else {
+        return "Input a valid answer"
+    }
+}
+console.log(getHumanChoice())
