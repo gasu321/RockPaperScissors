9b42349ed1a37ed3db7b672cb4f3424f11030b78
gasu321 <161547607+gasu321@users.noreply.github.com>
2024-05-18 03:18:38 -0700

    add function to return results of getHumanChoice and getComputerChoice

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Computer choice function
+
 function getComputerChoice() {
     let computerRoll = Math.random() * 100;
     if (computerRoll <= 33) {
@@ -9,7 +11,7 @@ function getComputerChoice() {
     }
 }
 
-console.log(getComputerChoice());
+// Human choice function
 
 function getHumanChoice() {
     let choice = prompt("Rock Paper or Scissors?").toLowerCase();
@@ -23,4 +25,31 @@ function getHumanChoice() {
         return "Input a valid answer"
     }
 }
-console.log(getHumanChoice())
+
+// Declare players score variables
+
+let humanScore = 0;
+let computerScore = 0;
+
+// Playing a round function
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice = getHumanChoice();
+    computerChoice = getComputerChoice();
+    console.log(computerChoice);
+    if (humanChoice === "rock" && computerChoice === "paper" ||
+        humanChoice === "paper" && computerChoice === "scissors" ||
+        humanChoice === "scissors" && computerChoice === "rock") {
+            return "You lose! lol!";
+    }else if (humanChoice === "rock" && computerChoice === "scissors" ||
+              humanChoice === "paper" && computerChoice === "rock" ||
+              humanChoice === "scissors" && computerChoice === "paper") {
+                  return "You win!";
+    }else {
+        return "It's a tie!"
+    }
+    
+
+}
+
+console.log(playRound())
